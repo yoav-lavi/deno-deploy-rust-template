1bda128dfd51bbab790d2fc122b13d3cf7f8cf5e
Yoav Lavi <yoavlavi122@gmail.com>
2022-07-03 21:11:58 +0200

    add test, panic hook, additional web-sys features

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ use web_sys::{Request, Response};
 
 #[wasm_bindgen]
 pub async fn handler(_request: Request) -> Result<Response, JsValue> {
+    console_error_panic_hook::set_once();
     Response::new_with_opt_str(Some("hello world!"))
 }
---
+++ tests/mod.rs
@@ -0,0 +1,21 @@
+use project_name::handler;
+use wasm_bindgen_test::*;
+use web_sys::{Request, RequestInit};
+
+#[wasm_bindgen_test]
+async fn deno_deploy_test() {
+    let mut request_init = RequestInit::new();
+
+    request_init.method("POST");
+
+    let request = Request::new_with_str_and_init("http://localhost:8000", &request_init).unwrap();
+
+    let response = handler(request).await.unwrap();
+
+    let response_body = wasm_bindgen_futures::JsFuture::from(response.text().unwrap())
+        .await
+        .unwrap()
+        .as_string();
+
+    assert_eq!(response_body, Some("hello world!".to_owned()));
+}
