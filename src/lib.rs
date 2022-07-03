022dc734b6e167476833cf4cabe45a9eef5eff67
Yoav Lavi <yoavlavi122@gmail.com>
2022-07-03 18:47:45 +0200

    simplify template

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ use web_sys::{Request, Response};
 
 #[wasm_bindgen]
 pub async fn handler(_request: Request) -> Result<Response, JsValue> {
-    Response::new_with_opt_str(Some("hello world!")))
+    Response::new_with_opt_str(Some("hello world!"))
 }
