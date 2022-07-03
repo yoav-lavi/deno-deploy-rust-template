2da3d52d003f30e7e99f806aa656458cc33d33d5
Yoav Lavi <yoavlavi122@gmail.com>
2022-07-03 18:46:29 +0200

    simplify template

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,8 @@
-mod utils;
-
 use js_sys::JSON;
-use utils::to_body;
 use wasm_bindgen::prelude::*;
 use web_sys::{Request, Response};
 
 #[wasm_bindgen]
-pub async fn handler(request: Request) -> Result<Response, JsValue> {
-    let request_body = to_body(request).await?;
-
-    let body_string = &JSON::stringify(&request_body)?;
-
-    Response::new_with_opt_str(body_string.as_string().as_deref())
+pub async fn handler(_request: Request) -> Result<Response, JsValue> {
+    Response::new_with_opt_str(Some("hello world!")))
 }
--- src/utils.rs
+++
@@ -1,7 +0,0 @@
-use wasm_bindgen::JsValue;
-use wasm_bindgen_futures::JsFuture;
-use web_sys::Request;
-
-pub async fn to_body(request: Request) -> Result<JsValue, JsValue> {
-    Ok(JsFuture::from(request.json()?).await?)
-}
